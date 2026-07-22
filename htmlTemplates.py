2936fb20d216c5f46a21c969e3731c4c59d99fd2
Tirth Patel <tirthk1510@gmail.com>
2026-07-22 14:45:49 +0530

    intial commit

---
+++ htmlTemplates.py
@@ -0,0 +1,58 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1rem;
+    border-radius: 0.5rem;
+    margin-bottom: 1rem;
+    display: flex;
+    max-width: 80%;
+}
+
+.chat-message.user {
+    background-color: #2b313e;
+    margin-left: auto;
+    flex-direction: row-reverse;
+}
+
+.chat-message.bot {
+    background-color: #475063;
+    margin-right: auto;
+    flex-direction: row;
+}
+
+.chat-message.avatar {
+    width: 15%;
+}
+
+.chat-message.avatar img {
+    max-width: 48px;
+    max-height: 48px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.chat-message .message {
+    width: 85%;
+    padding: 0 1rem;
+    color: #fff;
+}
+</style>
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/512/4712/4712027.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/512/1077/1077114.png">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
